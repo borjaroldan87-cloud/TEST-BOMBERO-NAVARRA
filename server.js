28e4643d38ddf704664d5edc6fd17ab44fd4040c
borjaroldan87-cloud <borjaroldan87@gmail.com>
2026-09-24 22:44:26 +0200

    Corrige parametros guardado grafico

--- server.js
+++ server.js
@@ -554,8 +554,9 @@ async function saveGraphicAnalysis(graphicRow, analysis){
         analysis_status
       )
       VALUES (
-        $1,$2,$3,$4,$5,$6,$7,$8,$9,
-        $10,$11,$12,$13,$14,$15,'analyzed'
+  $1,$2,$3,$4,$5,$6,$7,$8,$9,
+  $10,$11,$12,$13,$14,$15,$16
+)
       )
       ON CONFLICT (source_id, asset_index)
       DO UPDATE SET
